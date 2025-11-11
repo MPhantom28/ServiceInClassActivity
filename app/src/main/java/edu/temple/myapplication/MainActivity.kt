ffeab08fd469a2dc165b6bd17c4ad0fc50d55cd4
Tyrone Zhang <zhangtyrone2005@gmail.com>
2025-11-11 14:56:16 -0500

    worked on the button and binding service

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,79 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var timerBinder: TimerService.TimerBinder? = null
+    private var isBound = false
+
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
+    private lateinit var textView: TextView
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isBound = false
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        startButton = findViewById<Button>(R.id.startButton)
+        stopButton = findViewById<Button>(R.id.stopButton)
+        textView = findViewById<TextView>(R.id.textView)
+
+        Intent(this, TimerService::class.java).also {
+            bindService(it, connection, Context.BIND_AUTO_CREATE)
+        }
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            startService(Intent(this, TimerService::class.java))
+            if (isBound) {
+                if (!timerBinder!!.isRunning && !timerBinder!!.paused) {
+                    timerBinder!!.start(10)
+                    startButton.text = "Pause"
+                } else if (timerBinder!!.isRunning && !timerBinder!!.paused) {
+                    timerBinder!!.pause()
+                    startButton.text = "Resume"
+                } else if (timerBinder!!.paused) {
+                    timerBinder!!.pause()  // unpause
+                    startButton.text = "Pause"
+                }
+            }
         }
-        
+
+
+
         findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if (isBound) {
+                timerBinder!!.stop()
+                startButton.text = "Start"
+                textView.text = "Stopped"
+            }
+        }
 
         }
+
+//        override fun onDestroy() {
+//            // The service is no longer used and is being destroyed
+//        }
+
     }
-}
+
